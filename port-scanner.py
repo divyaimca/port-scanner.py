ecf4e4c30e1350b17332be2cfb285524ac26d6b8
kumarprd <40861429+kumarprd@users.noreply.github.com>
2018-07-26 14:40:53 +0530

    Create port-scanner.py

---
+++ port-scanner.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#Author: Priyadarshee Kumar
+#Port scanner
+
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+help="""
+Give the inputs as list here
+python port-scanner.py
+Enter list of target IPs: ["127.0.0.1","localhost"]
+Enter list of ports: [11,22,33,]
+"""
+
+target_hosts = input("Enter list of target IPs: ")
+target_ports = input("Enter list of ports: ")
+
+def scanner(target,port):
+    try:
+        sock.connect((target, port))
+        return True
+    except:
+        return False
+
+
+for host in target_hosts:
+	for portNumber in target_ports:
+    		if scanner(host,portNumber):
+        		print "Target Host: {} TCP Port: {} status : open.".format(_host,portNumber)
+    		else:
+        		print "Target Host: {} TCP Port: {} status : closed.".format(host,portNumber)
