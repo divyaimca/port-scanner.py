31f6d5d0ab2fc93a38df4fd995c8e68d7f4ae7dc
kumarprd <40861429+kumarprd@users.noreply.github.com>
2018-08-11 11:23:47 +0530

    Update port-scanner.py

--- port-scanner.py
+++ port-scanner.py
@@ -5,11 +5,7 @@
 import sys
 import socket
 
-##Pre_scanner : suppose port is opne in firewall, but service is stopped
-## Useful for doing a pre-validation of datacenter before build
-##
-
-def pre_scanner(port):
+def socket_server(port):
 	sock = socket.socket()
 	sock.bind(('0.0.0.0', port))
 	sock.listen(5)
@@ -21,7 +17,7 @@ def pre_scanner(port):
    		conn.close()
 
 
-def post_scanner(target,port):
+def socket_client(target,port):
 	try:
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         	status = sock.connect_ex((target, port))
@@ -35,10 +31,10 @@ def post_scanner(target,port):
 host = raw_input("Enter the hostname:")
 portNumber = int(raw_input("Enter the port:"))
 
-## pre_scanner should be triggered on the server side only
-##pre_scanner(portNumber) 
+## socket_server should be triggered on the server side only
+##socket_server(portNumber) 
 
-if post_scanner(host,portNumber) == 0:
+if socket_client(host,portNumber) == 0:
 	print "Target Host: {} TCP Port: {} status : open.".format(host,portNumber)
 else:
 	print "Target Host: {} TCP Port: {} status : closed.".format(host,portNumber)
